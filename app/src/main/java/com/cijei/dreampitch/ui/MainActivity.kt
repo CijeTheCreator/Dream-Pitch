6030a4addabf97b5025a8398ac54d47c4e6c4c12
Chijioke <osadebec98@gmail.com>
2022-06-27 10:00:17 +0100

    Mock ups finished yesterday, June 26th. Actual Development starts here.

---
+++ app/src/main/java/com/cijei/dreampitch/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cijei.dreampitch.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cijei.dreampitch.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
